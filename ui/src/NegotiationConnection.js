58f3d5653426000d20eaf7e0b91170f32a1e5106
shoelick <git@megascops.net>
2019-02-17 01:03:27 -0500

    Started on the web socket manager

--- ui/src/NegotiationConnection.js
+++ ui/src/NegotiationConnection.js
@@ -3,20 +3,53 @@ import adapter from 'webrtc-adapter';
 class NegotiationConnection {
 
     constructor(stun=null) {
+        this.conn = new WebSocket("ws://" + document.location.host +
+            "/ws");
+
+        this.conn.onclose = function (evt) {
+            console.log("Web socket closed; trying for a new stream...");
+            // TODO is this supposed to be called by someone else?
+            //grabNewStream();
+        }
+
+        this.conn.onmessage = function (evt) {
+            var msg = JSON.parse(evt.data);
+
+            switch(msg.command=) {
+
+                    this.updateViewerCountMsg) ?
+
+        };
+
+        this.stream_identifier = "";
+
+        // constants
+        this.nextViewingMsg = 2;
+        this.nextViewingRespMsg = 3;
+        this.startStreamingMsg = 4;
+        this.updateViewerCountMsg = 5;
+
     }
 
-    // Promise<Void>
-    async onInit() {
-        
+    writeMessage(command, arg) {
+        var msg = { command: command, arg: arg };
+        this.conn.send(JSON.stringify(msg));
     }
 
     // Promise<RPCPeerConnection>
     async grabNewStream() {
 
+        // Register ourselves as a viewer and get a new stream
+
+
     }
 
     // Promise<Void>
     async startBroadcast() {
+
+        // First get stream identifier from STUN server
+
+        // Register ourselves as a streamer
 
     }
 
