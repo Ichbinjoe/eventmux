c39646b36fc58a7be03824d442ece66dc601ac45
Joseph Hirschfeld <joe@ibj.io>
2019-02-17 03:40:11 -0500

    pls

--- eventmux_server/main.go
+++ eventmux_server/main.go
@@ -10,19 +10,6 @@ import (
 
 var addr = flag.String("addr", ":80", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
-
 func main() {
 	flag.Parse()
 	hub := newHub()
@@ -34,7 +21,7 @@ func main() {
 	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./test_ui/")))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../ui/build")))
 
 	http.ListenAndServe(":80", r)
 
